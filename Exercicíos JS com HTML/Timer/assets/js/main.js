94db5348a61424b2e699e697e4f0be577969c50c
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-13 23:19:10 -0300

    add a exercises

---
+++ Exercicíos JS com HTML/Timer/assets/js/main.js
@@ -0,0 +1,36 @@
+function getTimeFromSeconds(segundos){
+  const data = new Date(segundos * 1000);
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  });
+}
+
+const relogio = document.querySelector('.relogio');
+let segundos = 0;
+let timer;
+
+function startTimer() {
+  timer = setInterval(() => {
+    segundos++;
+    relogio.innerHTML = getTimeFromSeconds(segundos);
+  }, 1000)
+}
+
+document.addEventListener('click', e => {
+  const el = e.target;
+
+  if(el.classList.contains('zerar')) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00'
+    segundos = 0;
+  } else if (el.classList.contains('pausar')) {
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+  } else {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    startTimer();
+  }
+});
