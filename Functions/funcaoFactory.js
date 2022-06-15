84af1b333b588c61a18ae18757e87c1e3533e951
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-14 22:18:53 -0300

    add exercise

--- Functions/funcaoFactory.js
+++ Functions/funcaoFactory.js
@@ -2,9 +2,21 @@ function criaPessoa(nome, sobrenome, a, p) {
   return{
     nome,
     sobrenome,
+    // Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      console.log(valor);
+    },
+
     fala(assunto) {
       return `${nome} está ${assunto}.`;
     },
+
     altura: a,
     peso: p,
     // Getter
@@ -14,11 +26,14 @@ function criaPessoa(nome, sobrenome, a, p) {
     }
   };
 }
+
 const p1 = criaPessoa('Douglas', 'Fernandes', 1.8, 80);
 const p2 = criaPessoa('Maria', 'Tania', 1.6, 42)
 console.log(p1.fala('falando sobre JS'));
-console.log(p1.nome, p1.sobrenome);
-console.log(p2.nome, p2.sobrenome);
+console.log("######");
+console.log(p1.nomeCompleto);
+console.log(p2.nomeCompleto);
 console.log("#######");
 console.log(p1.imc);
 console.log(p2.imc);
+p1.nomeCompleto = 'Jackson Philippe Silva';
