5e9a5f89f31f342e180a560bb7ec08aa390135e5
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-22 21:45:55 -0300

    reduce js exercise

--- Arrays/reduce.js
+++ Arrays/reduce.js
@@ -7,6 +7,22 @@
 */
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce(function(acumulador, valor, indice, array){
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+  acumulador += valor;
+  return acumulador;
+}, 0);
+console.log(total);
 
-});
+console.log('################################');
+const pares = numeros.reduce(function(acumulador, valor) {
+  if(valor % 2 === 0) acumulador.push(valor);
+  return acumulador;
+}, []);
+console.log(pares);
+console.log('################################');
+
+const dobro = numeros.reduce(function(acumulador, valor) {
+  acumulador.push(valor * 2);
+  return acumulador
+}, []);
+console.log(dobro);
