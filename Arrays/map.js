913de29900d68f7ebf7fa21c6a627f29eac630e1
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-22 21:17:49 -0300

    update map exercise

--- Arrays/map.js
+++ Arrays/map.js
@@ -1,7 +1,7 @@
 //map(valor, indice, array);
 
-//Dobre os numeros = * 2
-//Triplicar os numeros = * 3
+//Dobre os numeros = valor * 2
+//Triplicar os numeros = valor * 3
 
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 console.table(numeros);
@@ -23,15 +23,26 @@ const pessoas = [
   { nome: 'Rosana', idade: 32 },
   { nome: 'Wallace', idade: 47 }
 ];
+console.table(pessoas);
 
 const nomes = pessoas.map(valor => valor.nome);
 console.log(nomes);
 
 const removeNome = pessoas.map(valor => ({ idade: valor.idade }));
 console.log(removeNome);
-
+/* altera o OBJETO
 const addId = pessoas.map((valor, indice) => {
   valor.id = (indice +1) * 1000;
   return valor;
 });
 console.log(addId);
+console.table(pessoas);
+*/
+// Não altera o objeto
+const addId = pessoas.map((valor, indice) => {
+  const newValor = {...valor};
+  newValor.id = indice;
+  return newValor;
+});
+console.table(addId);
+console.table(pessoas);
