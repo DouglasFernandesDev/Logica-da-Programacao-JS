05866fae1d7a34ed3573847755a1b436536600b2
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-23 20:42:11 -0300

    quizz OBC with objects

---
+++ Objetos/quizzObjs.js
@@ -0,0 +1,7 @@
+let spaceship = {};
+  spaceship.name = "Ártemis";
+  spaceship.type = "Descoberta";
+  spaceship["crew quantity"] = 6;
+  spaceship["isHitched"] = false;
+
+  console.log(spaceship);
