f2280dd75c9a91567205d966a0c1c9c41aaf19f1
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-06-23 21:13:21 -0300

    update objects and arrays

--- Objetos/objsEArrays.js
+++ Objetos/objsEArrays.js
@@ -18,3 +18,14 @@ let navispacial = [
 navispacial.forEach(navispacial => {
   console.log(navispacial.name + " tem " + navispacial.crewQuantity + " tripulantes");
 });
+
+console.log();
+let navespacial = {
+  name: "Golias",
+  maxCrew: 20,
+  capitain: {
+    name: "Hugo Trent",
+    age: 37
+  }
+}
+console.log(navespacial.capitain.name);
