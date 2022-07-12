450372ee4d6d6da557ad5cb8a4d43a5966ecf3b8
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-11 22:55:51 -0300

    add exercise of classes on js

---
+++ POO/validandoCPF.js
@@ -0,0 +1,18 @@
+//  705.484.450-52   070.987.720-03
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    });
+  }
+    valida() {
+      if(!this.cpfLimpo) return false;
+      if(typeof cpfLimpo !== 'string') return false;
+      return 'Cheguei aqui';
+    }
+}
+const validacpf = new ValidaCPF('070.987.720-03');
+console.log(validacpf.valida());
