d82526f611e48ec64cb7da9b6603d05947d83362
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-16 00:33:39 -0300

    #1015 URI/Beecrowd exercise

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -5,7 +5,11 @@ const X = A + B
 */
 const input = require("fs").readFileSync("stdin", "utf-8");
 
-const [A, B] = input.split('\n').map(num => Number(num));
+const valor = input.split('\n');
 
-const kmL = A / B;
-console.log(`${kmL.toFixed(3)} km/l`);
+const [x1, y1] = valor[0].split(' ');
+const [x2, y2] = valor[1].split(' ');
+
+const distancia = Math.sqrt(((x2 - x1) ** 2 + (y2 - y1) ** 2));
+
+console.log(distancia.toFixed(4));
