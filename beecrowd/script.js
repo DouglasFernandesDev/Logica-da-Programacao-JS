f17167d80ea364c1e82c870ff5c35097eda6a715
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-16 00:05:53 -0300

    #1014 beecrowd

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -5,4 +5,7 @@ const X = A + B
 */
 const input = require("fs").readFileSync("stdin", "utf-8");
 
-const [a, b] = input.split(" ")
+const [A, B] = input.split('\n').map(num => Number(num));
+
+const kmL = A / B;
+console.log(`${kmL.toFixed(3)} km/l`);
