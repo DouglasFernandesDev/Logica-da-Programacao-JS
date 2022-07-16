6dcf9d219259b4e65164e92e5f5dbe29e591e08e
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-16 00:54:16 -0300

    #1016 URI/Beecrowd

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -5,11 +5,6 @@ const X = A + B
 */
 const input = require("fs").readFileSync("stdin", "utf-8");
 
-const valor = input.split('\n');
+const qtdMinutos = (60 * input) / 30;
 
-const [x1, y1] = valor[0].split(' ');
-const [x2, y2] = valor[1].split(' ');
-
-const distancia = Math.sqrt(((x2 - x1) ** 2 + (y2 - y1) ** 2));
-
-console.log(distancia.toFixed(4));
+console.log(qtdMinutos + " minutos");
