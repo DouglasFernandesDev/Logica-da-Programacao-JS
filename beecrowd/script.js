a004ad661f8795f72f0dcc93c7394fdb18d446d7
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-16 01:34:14 -0300

    #1018 URI/Beecrowd

--- beecrowd/script.js
+++ beecrowd/script.js
@@ -4,8 +4,35 @@ const B = parseInt(lines.shift());
 const X = A + B
 */
 const input = require("fs").readFileSync("stdin", "utf-8");
-const [A , B] = input.split('\n');
+let lines = Number(input);
 
-const totalKm = (A * B) / 12;
+const string = ' nota(s) de R$ ';
+console.log(lines);
 
-console.log(totalKm.toFixed(3));
+let qtdNotas = parseInt(lines / 100);
+console.log(`${qtdNotas + string + "100,00"}`);
+lines = lines % 100;
+
+qtdNotas = parseInt(lines / 50);
+console.log(`${qtdNotas + string + "50,00"}`);
+lines = lines % 50;
+
+qtdNotas = parseInt(lines / 20);
+console.log(`${qtdNotas + string + "20,00"}`);
+lines = lines % 20;
+
+qtdNotas = parseInt(lines / 10);
+console.log(`${qtdNotas + string + "10,00"}`);
+lines = lines % 10;
+
+qtdNotas = parseInt(lines / 5);
+console.log(`${qtdNotas + string + "5,00"}`);
+lines = lines % 5 
+
+qtdNotas = parseInt(lines / 2);
+console.log(`${qtdNotas + string + "2,00"}`);
+lines = lines % 2
+
+qtdNotas = parseInt(lines / 1);
+console.log(`${qtdNotas + string + "1,00"}`);
+lines = lines % 1;
