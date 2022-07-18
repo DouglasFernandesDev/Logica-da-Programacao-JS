92ce287a271b1ae11f5192e2d3d5c896dc6af92b
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-18 17:13:06 -0300

    update xmlHttpRequest.js

---
+++ Async&Await/xmlHttpRequest/xmlHttpRequest.js
@@ -0,0 +1,39 @@
+const request = obj => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(obj.method, obj.url, true);
+    xhr.send();
+  
+    xhr.addEventListener('load', () => {
+      xhr.status >= 200 && xhr.status < 300 ?
+      resolve(xhr.responseText) : reject(xhr.statusText);
+    });
+  })
+};
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if(tag === 'a') {
+    e.preventDefault();
+    loadPage(el);
+  }
+})
+
+function loadPage(el) {
+  const href = el.getAttribute('href');
+  
+  request({
+    method: 'GET',
+    url: href,
+  }).then(res => {
+    loadResult(res);
+  }).catch(err => console.log(err));
+
+}
+
+function loadResult(res) {
+  const result = document.querySelector('.resultado');
+  result.innerHTML = res;
+}
