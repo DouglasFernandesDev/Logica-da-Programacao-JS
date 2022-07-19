c3d3f7aceeaa76091572c94d054cb8bdd67a705a
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-18 23:51:09 -0300

    update webpack.config.js

--- Modulos/webpack/webpack.config.js
+++ Modulos/webpack/webpack.config.js
@@ -8,7 +8,16 @@ module.exports = {
     filename: 'bundle.js'
 },
 module : {
-
+  rules: [{
+    exclude: /node_modules/,
+    test: /\.js$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/env']
+      }
+    }
+  }]
 },
 devtool: 'source-map'
 };
