1b725fa9eedd069789a0369aab31458abe9aa5a8
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-07-18 21:57:42 -0300

    update main.js

--- Async&Await/fetch-axios/assets/js/main.js
+++ Async&Await/fetch-axios/assets/js/main.js
@@ -8,16 +8,20 @@ fetch('pessoas.json')
 function carregaElementosNaPagina(json) {
   const table = document.createElement('table');
 
-  for(let pessoa of json) {
+  for(const pessoa of json) {
     const tr = document.createElement('tr');
 
     let td1 = document.createElement('td');
-    td1.innerHTML = pessoa.nome;
+    td1.innerHTML = 'Nome: ' + pessoa.nome;
     tr.appendChild(td1);
 
     let td2 = document.createElement('td');
-    td2.innerHTML = pessoa.idade;
+    td2.innerHTML = 'Idade: ' +pessoa.idade;
     tr.appendChild(td2);
+
+    let td3 = document.createElement('td');
+    td3.innerHTML = `Salário R$${pessoa.salario}`;
+    tr.appendChild(td3);
 
     table.appendChild(tr);
   }
